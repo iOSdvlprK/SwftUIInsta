d4c5e350096d975d88026253c40799036da33c08
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-08 20:48:10 +0900

    Initial Commit

---
+++ SwftUIInsta/SwftUIInstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwftUIInstaApp.swift
+//  SwftUIInsta
+//
+//  Created by joe on 2023/12/08.
+//
+
+import SwiftUI
+
+@main
+struct SwftUIInstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
