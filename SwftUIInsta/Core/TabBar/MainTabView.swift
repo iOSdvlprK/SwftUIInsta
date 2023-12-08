53e6ef7ffe27a89acaea292911092f966fd580da
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-08 21:15:47 +0900

    tab bar setup

---
+++ SwftUIInsta/Core/TabBar/MainTabView.swift
@@ -0,0 +1,46 @@
+//
+//  MainTabView.swift
+//  SwftUIInsta
+//
+//  Created by joe on 2023/12/08.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .tint(.black)  // 'accentColor' will be deprecated
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
