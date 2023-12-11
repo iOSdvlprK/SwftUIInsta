0f825146108ecbb784bec6d59c70675f7aa9589f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-11 21:17:40 +0900

    user profile navigation

---
+++ SwftUIInsta/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,101 @@
+//
+//  CurrentUserProfileView.swift
+//  SwftUIInsta
+//
+//  Created by joe on 2023/12/11.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    private var gridItems: [GridItem] = [
+        GridItem(.flexible(), spacing: 1),
+        GridItem(.flexible(), spacing: 1),
+        GridItem(.flexible(), spacing: 1)
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                // header
+                VStack(spacing: 10) {
+                    // pic and stats
+                    HStack {
+                        Image("black-panther-1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            
+                            UserStatView(value: 12, title: "Followers")
+                            
+                            UserStatView(value: 24, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    // name and bio
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Chadwick Bozeman")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("Wakanda Forever")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    // action button
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(Color(.label))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color(.systemGray), lineWidth: 1)
+                            }
+                    }
+                    
+                    Divider()
+                }
+                
+                // post grid view
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0...15, id: \.self) { index in
+                        Image("blackpanther")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(Color(.label))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CurrentUserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentUserProfileView()
+    }
+}
