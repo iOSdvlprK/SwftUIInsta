ab9dbac0d2600d4cb6c42e2f4b9e718f12df7e68
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-11 19:19:35 +0900

    User data model and mock user data

---
+++ SwftUIInsta/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  SwftUIInsta
+//
+//  Created by joe on 2023/12/11.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    var username: String
+    var profileImageUrl: String?
+    var fullname: String?
+    var bio: String?
+    let email: String
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: UUID().uuidString, username: "batman", profileImageUrl: "batman", fullname: "Bruce Wayne", bio: "Gotham's Dark Knight", email: "batman@gmail.com"),
+        .init(id: UUID().uuidString, username: "venom", profileImageUrl: "venom", fullname: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
+        .init(id: UUID().uuidString, username: "ironman", profileImageUrl: "ironman", fullname: "Tony Stark", bio: "Playboy & Billionaire", email: "ironman@gmail.com"),
+        .init(id: UUID().uuidString, username: "blackpanther", profileImageUrl: "blackpanther", fullname: nil, bio: "Wakanda Forever", email: "blackpanther@gmail.com"),
+        .init(id: UUID().uuidString, username: "spiderman", profileImageUrl: "spiderman", fullname: "Peter Parker", bio: "Test bio", email: "spiderman@gmail.com")
+    ]
+}
