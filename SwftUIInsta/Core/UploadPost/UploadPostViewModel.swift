23c13d2514f554c14b32062e9d300bafb7e82741
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-12 03:37:43 +0900

    upload post view and view model; managing tab index

---
+++ SwftUIInsta/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  UploadPostViewModel.swift
+//  SwftUIInsta
+//
+//  Created by joe on 2023/12/12.
+//
+
+import SwiftUI
+import PhotosUI
+
+@MainActor
+class UploadPostViewModel: ObservableObject {
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet { Task { await loadImage(fromItem: selectedImage) } }
+    }
+    @Published var postImage: Image?
+    
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item = item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.postImage = Image(uiImage: uiImage)
+    }
+}
