98a76933af7bd355a30a6f07d984cbdd79976217
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-14 23:55:01 +0900

    register a user

---
+++ SwftUIInsta/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationViewModel.swift
+//  SwftUIInsta
+//
+//  Created by joe on 12/14/23.
+//
+
+import Foundation
+
+class RegistrationViewModel: ObservableObject {
+    @Published var username = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    func createUser() async throws {
+        try await AuthService.shared.createUser(email: email, password: password, username: username)
+    }
+}
