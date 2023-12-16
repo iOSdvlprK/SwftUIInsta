fb5677eb4e8ff089dc7ce5042b530fc3944fddf3
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-17 04:04:14 +0900

    fetch users from Firestore

---
+++ SwftUIInsta/Core/Search/ViewModel/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  SwftUIInsta
+//
+//  Created by joe on 12/17/23.
+//
+
+import Foundation
---
+++ SwftUIInsta/Services/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  SwftUIInsta
+//
+//  Created by joe on 12/17/23.
+//
+
+import Foundation
